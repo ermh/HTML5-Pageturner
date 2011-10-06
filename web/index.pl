2d2a5e28c83c88803c5b6c44dd5e755492fe9d0f
Erik Martino Hansen <erik.martino@gmail.com>
2011-10-06 23:54:28 +0200

    seems to work ok now

--- web/index.pl
+++ web/index.pl
@@ -1,33 +1,42 @@
 #!/usr/bin/perl -w
 
-($lw,$lh) = (2569,3248);
 $l2n = 72/300;
+$l2s = 15/300;
+
+($lw,$lh) = (2569,3248);
 ($nw,$nh) = (int($lw*$l2n),int($lh*$l2n));
+($sw,$sh) = (int($lw*$l2s),int($lh*$l2s));
+
+print "large:  $lw,$lh\n";
+print "normal: $nw,$nh\n";
+print "small:  $sw,$sh\n";
+
 $tilesize=400;
 
-@images = ("S001CO103.jpg",
-			"S002CO103.jpg",
-			"S003CO103.jpg",
-			"S004CO103.jpg",
-			"S005CO103.jpg",
-			"S006CO103.jpg",
-			"S007CO103.jpg",
-			"S008CO103.jpg",
-			"S009CO103.jpg",
-			"S010CO103.jpg",
-			"S011CO103.jpg",
-			"S012CO103.jpg",
-			"S013CO103.jpg",
-			"S014CO103.jpg",
-			"S015CO103.jpg",
-			"S016CO103.jpg",
-			"S017CO103.jpg",
-			"S018CO103.jpg",
-			"S019CO103.jpg",
-			"S020CO103.jpg",
-			"S021CO103.jpg",
-			"S022CO103.jpg",
-			"S023CO103.jpg");
+@images = (
+    "S001CO103.jpg",
+    "S002CO103.jpg",
+    "S003CO103.jpg",
+    "S004CO103.jpg",
+    "S005CO103.jpg",
+    "S006CO103.jpg",
+    "S007CO103.jpg",
+    "S008CO103.jpg",
+    "S009CO103.jpg",
+    "S010CO103.jpg",
+    "S011CO103.jpg",
+    "S012CO103.jpg",
+    "S013CO103.jpg",
+    "S014CO103.jpg",
+    "S015CO103.jpg",
+    "S016CO103.jpg",
+    "S017CO103.jpg",
+    "S018CO103.jpg",
+    "S019CO103.jpg",
+    "S020CO103.jpg",
+    "S021CO103.jpg",
+    "S022CO103.jpg",
+    "S023CO103.jpg");
 
 open(IMGLIST,">images.js");
 print IMGLIST "var images = [\n";
@@ -40,32 +49,67 @@ print IMGLIST "];\n";
 close(IMGLIST);
 
 
-if (0) {
+
+if (1) {
+open(APPCACHE,">test.appcache");
 
 system("mkdir -p images/large");
 system("mkdir -p images/normal");
+system("mkdir -p images/small");
+
+print APPCACHE <<END;
+CACHE MANIFEST
+
+NETWORK:
+*
+
+CACHE:
+http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+index.html
+script.js
+style.css
+END
 
 for($i=0; $i<$#images; $i++) {
-	$cmd = "convert -filter Quadratic -resize ${nw}x${nh} images/large/$images[$i] images/normal/$images[$i]";
-	print STDERR "$cmd\n";
-	system($cmd);
+	# Normal
+        $cmd = "convert -filter Quadratic -resize ${nw}x${nh} images/large/$images[$i] images/normal/$images[$i]";
+	print STDERR "generate normal size (72dpi) $cmd\n";
+	#system($cmd);
 	print STDERR "DONE\n";
+	
+	print APPCACHE "images/normal/$images[$i]\n";
+	print APPCACHE "images/small/$images[$i]\n";
+
+        # Small
+	$cmd = "convert -filter Quadratic -resize ${sw}x${sh} images/large/$images[$i] images/small/$images[$i]";
+	print STDERR "generate small size (12dpi) $cmd\n";
+	#system($cmd);
+	print STDERR "DONE\n";
+
 	for($dx=0; $dx<$lw; $dx+=${tilesize}) {
 		for($dy=0; $dy<$lh; $dy+=${tilesize}) {
 			$dx2 = $dx+${tilesize} < $lw ? $dx+${tilesize} : $lw;
 			$dy2 = $dy+${tilesize} < $lh ? $dy+${tilesize} : $lh;
+			
 			$w = $dx2-$dx;
 			$h = $dy2-$dy;
-			$cmd = "mkdir -p images/tiles/${dx},${dy}";
+
+			# $cmd = "mkdir -p images/tiles/${dx},${dy}";
 			# print STDERR "$cmd\n";
-			system($cmd);
+			# system($cmd);
+			
 			$cmd = "convert -crop ${w}x${h}+${dx}+${dy} -quality 50 images/large/$images[$i] images/tiles/${dx},${dy}/$images[$i]";
 			print STDERR "$cmd\n";
 			system($cmd);
+
+			print APPCACHE "images/tiles/${dx},${dy}/$images[$i]\n";
 		}
 	}
 }
+close(APPCACHE);
 }
+
+
 			
 print <<END;
 <html>
--- web/script.js
+++ web/script.js
@@ -1,3 +1,30 @@
+var images = [
+    "S001CO103.jpg",
+    "S002CO103.jpg",
+    "S003CO103.jpg",
+    "S004CO103.jpg",
+    "S005CO103.jpg",
+    "S006CO103.jpg",
+    "S007CO103.jpg",
+    "S008CO103.jpg",
+    "S009CO103.jpg",
+    "S010CO103.jpg",
+    "S011CO103.jpg",
+    "S012CO103.jpg",
+    "S013CO103.jpg",
+    "S014CO103.jpg",
+    "S015CO103.jpg",
+    "S016CO103.jpg",
+    "S017CO103.jpg",
+    "S018CO103.jpg",
+    "S019CO103.jpg",
+    "S020CO103.jpg",
+    "S021CO103.jpg",
+    "S022CO103.jpg",
+    "S023CO103.jpg"
+];
+
+
 function largeurl(id) {
     
 }
@@ -26,9 +53,8 @@ function showMagnifier(img,mousepos) {
 	var children = $(magnifier).children();
 	children.remove();
 	var pos = {left: mousepos.x - tilesize/2, top: mousepos.y - tilesize/2};
-	magnifier.position(pos);
 	
-	var imgpos = $(img).offset();
+        var imgpos = $(img).offset();
 	var nsize = normalsize(img);
 	var lsize = largesize(img);
 
@@ -38,38 +64,60 @@ function showMagnifier(img,mousepos) {
 	
 	var tile;
 	for(var i=0; i<tiles.length; i++) {
-        tile = tiles[i];
-  		var t = $("<img></img>");
-		t.css("position","relative");
-		t.css("left","5px");
-		t.css("top",tile.uly);
-		t.css({left: tile.ulx, top: tile.uly, width: tile.width, height: tile.height});
-		magnifier.append(t);
+            tile = tiles[i];
+            var t = $("<img></img>");
+            t.css("position","absolute");
+            t.css("left",tile.left);
+            t.css("top",tile.top);
+            t.css("width", tile.width);
+            t.css("height", tile.height);
+            t.attr("src",tile.url);
+            magnifier.append(t);
 	}
+	magnifier.css("position", "absolute");
+	magnifier.css("left", pos.left);
+    magnifier.css("top", pos.top);
 	
 	magnifier.css("display","block");
 	magnifier.show();
 }
+
+alert("load");
 
 function tilesAround(img,lpos,magsize) {
 	var upperLeft = {x: Math.round(lpos.x - magsize / 2), y: Math.round(lpos.y - magsize / 2)};
 	var lowerRight = {x: lpos.x + magsize / 2, y: lpos.y + magsize / 2};
 	var lsize = largesize(img);
 	var tiles=[];
-	
-	for(var x = upperLeft.x; x - tilesize < lowerRight.x; x += tilesize) {
-		for(var y = upperLeft.y; y - tilesize < lowerRight.y; y += tilesize) {
-			var width = Math.min(x+tilesize,lsize.width) - x;
-			var height = Math.min(y+tilesize,lsize.height) - y;
-			tiles.push( {ix: x, iy: y, ulx: x-upperLeft.x, uly: y-upperLeft.y, width: width, height: height, url: } );
-		}			
-	}
-
+        
+        var upperLeftTile = {
+            x: Math.floor(upperLeft.x / tilesize), 
+            y: Math.floor(upperLeft.y / tilesize)
+        }
+        var lowerRightTile = {
+            x: Math.floor(lowerRight.x / tilesize), 
+            y: Math.floor(lowerRight.y / tilesize)
+        }
+        
+        var lsize = largesize(img);
+        	
+        for(var tiy = upperLeftTile.y; tiy <= lowerRightTile.y; tiy++) {
+            for(var tix = upperLeftTile.x; tix <= lowerRightTile.x; tix++) {
+                var left = tix * tilesize - upperLeft.x;
+                var top = tiy * tilesize - upperLeft.y;     
+                var tilename = (tix * tilesize)+","+(tiy * tilesize);
+                var width = Math.min(lsize.width - tix * tilesize,tilesize);
+                var height = Math.min(lsize.height - tiy * tilesize,tilesize);
+                alert(""+width);
+                var url = "images/tiles/"+tilename+"/"+basename(img);
+ 				tiles.push({left: left, top: top, url: url, width: width, height: height});
+            }
+        }
 	return tiles;
 }
 
 function basename(img) {
-	return $(img).attr('src').replace('images/.*/','');
+	return $(img).attr('src').replace('images/normal/','');
 }
 
 function tile(img,x,y) {
